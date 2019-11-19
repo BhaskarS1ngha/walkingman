a953adf964d079b8e249dc2bff5c648d42c1cf49
Bhaskar-iot <bhaskarsingha7@gmail.com>
2019-11-20 01:46:39 +0530

    stickman animated. threading in next commit

--- walkingman/walkingman.cpp
+++ walkingman/walkingman.cpp
@@ -6,15 +6,27 @@
 #include <GLFW/glfw3.h>
 #include <Windows.h>
 #include<memory>
+#include<thread>
+#include<chrono>
 
+#define RAND_MAX  500
 using namespace std;
 
 const GLint WIDTH = 800, HEIGHT = 600;
+GLFWwindow* window;
+
 
 class DrawingObject
 {
 public:
 	float x, y, size;
+	DrawingObject(float xc, float yc)
+	{
+		x = xc;
+		y = yc;
+		size = 10;
+
+	}
 	DrawingObject(float xc, float yc, float sizeVal)
 	{
 		x = xc;
@@ -34,6 +46,126 @@ public:
 	}
 };
 
+class man : public DrawingObject {
+public:
+	man(float cx, float cy=270, float r=5,int  numSegments=10) :DrawingObject(cx, cy) {
+		seg = (numSegments) * 2;
+		vrtc_head = new float[seg];
+		vrtc_body = new float[4];
+		vrtc_leg2 = new float[2];
+		vrtc_leg1 = new float[2];
+		rad = r;
+		genCoords(x);
+	}
+	void genCoords(float x) {
+		vrtc_body[0] = vrtc_body[2] = vrtc_leg1[0] = vrtc_leg2[0] = x;
+		vrtc_body[1] = y - rad;
+		vrtc_body[3] = vrtc_body[1] - 30;
+		vrtc_leg1[1] = vrtc_leg2[1] = 210;
+		vrtc_leg1[0] = x - spread;
+		vrtc_leg2[0] = x + spread;
+		int n = 10 + rand();
+		n /= 10;
+		if (n < 1)
+		{
+			Tvector = -1;
+			delay = -1 * n;
+		}
+		else
+		{
+			Tvector = 1;
+			delay = n;
+		}
+		memset(vrtc_head, 0x0, sizeof(vrtc_head) * seg);
+		int j;
+		for (int i = 0, j = 0; i < seg; i += 2, j++)
+		{
+			float theta = 2.0f * 3.1415926f * float(j) / float(seg/2);//get the current angle
+
+			float tx = (rad * cosf(theta));//calculate the x component
+			float ty = (rad * sinf(theta));//calculate the y component
+			vrtc_head[i] = tx + x;
+			vrtc_head[i + 1] = ty + y;
+		}
+
+	}
+	void set_speed(float s)
+	{
+		Tvector = s;
+	}
+	void drawMan()
+	{
+		glLineWidth(1.2);
+		glBegin(GL_LINE_LOOP);
+		glLineWidth(2);
+		for(int i=0;i<seg;i+=2)
+			glVertex2f(vrtc_head[i],vrtc_head[i+1]);
+		glEnd();
+		glBegin(GL_LINES);
+		glLineWidth(2);
+		for (int i = 0; i < 4; i += 2)
+			glVertex2f(vrtc_body[i], vrtc_body[i + 1]);
+		glVertex2f(vrtc_body[2], vrtc_body[3]);
+		glVertex2f(vrtc_leg1[0], vrtc_leg1[1]);
+		glVertex2f(vrtc_body[2], vrtc_body[3]);
+		glVertex2f(vrtc_leg2[0], vrtc_leg2[1]);
+		
+		glEnd();
+	}
+	void posUpdate()
+	{
+		
+		//while (!glfwWindowShouldClose(window))
+		//{
+			if (x > 800)
+			{
+				resetPos();
+			}
+			else{
+				x += Tvector;
+				for (int i = 0; i < seg; i += 2)
+				{
+					vrtc_head[i] += Tvector;
+				}
+				for (int i = 0; i < 4; i += 2)
+				{
+					vrtc_body[i] += Tvector;
+				}
+				leg_update();
+			}
+			
+
+
+		//}
+	}
+	void resetPos()
+	{
+		x = 1;
+		genCoords(x);
+		
+	}
+
+private:
+	const float spread = 10;
+	int seg,dir =2,rad;
+	float Tvector,delay;
+	float *vrtc_head, *vrtc_body, *vrtc_leg1, *vrtc_leg2;
+	
+	void leg_update()
+	{
+		
+		vrtc_leg1[0] += Tvector;
+		vrtc_leg2[0] += Tvector;
+		
+		if (vrtc_leg1[0] == vrtc_leg2[0] || vrtc_leg1[0]>x+spread||vrtc_leg1[0]<x-spread)
+		{
+			dir *= -1;
+		}
+		vrtc_leg1[0] += dir;
+		vrtc_leg2[0] -= dir;
+		
+	}
+};
 
 
 
@@ -63,11 +195,19 @@ public:
 	{
 		glColor3f(0.0f, 0.0f, 0.0f);
 		glLineWidth(1.2);
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glVertexPointer(3, GL_FLOAT, 0, vrtc);
-		glDrawArrays(GL_POLYGON, 0, 4);
-		glDrawArrays(GL_TRIANGLES, 2, 3);
-		glDisableClientState(GL_VERTEX_ARRAY);
+		glBegin(GL_LINE_LOOP);
+		for (int i = 0; i < 7; i+=3)
+		{
+			glVertex3f(vrtc[i], vrtc[i + 1],vrtc[i+2]);
+			glVertex3f(vrtc[i + 3], vrtc[i + 4],vrtc[i+5]);
+		}
+		glEnd();
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(vrtc[6], vrtc[7]);
+		glVertex2f(vrtc[9], vrtc[10]);
+		glVertex2f(vrtc[12], vrtc[13]);
+		glEnd();
+
 	}
 	  ~house(){
 		  delete[] vrtc;
@@ -120,17 +260,15 @@ public:
 	void drawTree()
 	{
 		glLineWidth(1.2);
-		glEnableClientState(GL_VERTEX_ARRAY);
-		//cout << "dr1" << endl;
-		glVertexPointer(2, GL_FLOAT, 0, vrtc);
-		glDrawArrays(GL_POLYGON, 0, 11);
-		glDisableClientState(GL_VERTEX_ARRAY);
+		glBegin(GL_POLYGON);
+		for (int i = 0; i <= 18; i+=2)
+		{
+			glVertex2f(vrtc[i], vrtc[i + 1]);
+			glVertex2f(vrtc[i + 2], vrtc[i + 3]);
+		}
+		glEnd();
 	//	cout << "dr2" << endl;
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glVertexPointer(2, GL_FLOAT, 0, vrtc);
-		glDrawArrays(GL_POLYGON, 0, 11);
-		glDisableClientState(GL_VERTEX_ARRAY);
-	//	cout << "dr2" << endl;
+	
 		glBegin(GL_LINES);
 		glLineWidth(2);
 		glVertex2f(trunk[0], trunk[1]);
@@ -202,16 +340,34 @@ void generateObject(float x) {
 
 }
 
+void DrawCircle(float cx, float cy, float r, int num_segments)
+{
+	glBegin(GL_LINE_LOOP);
+	for (int ii = 0; ii < num_segments; ii++)
+	{
+		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
+
+		float x = r * cosf(theta);//calculate the x component
+		float y = r * sinf(theta);//calculate the y component
+
+		glVertex2f(x + cx, y + cy);//output vertex
+
+	}
+	glEnd();
+}
 
 
 int main()
 {
 	glfwInit();
-	float x = 0;
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "walking men", nullptr, nullptr);
+	float x = 10,cx=10;
+	window = glfwCreateWindow(WIDTH, HEIGHT, "walking men", nullptr, nullptr);
 	int screenWidth, screenHeight;
 	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
-
+//	glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
+	man m1 = man(100);
+	m1.set_speed(2.0f);
 	 //house h1 = house(400,300,100);
 	// house* hp = &h1;
 
@@ -234,6 +390,12 @@ int main()
 
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	while (x <= 1060)
+	{
+		generateObject(x);
+		x += 150;
+	}
+	x = 1060;
 	while (!glfwWindowShouldClose(window))
 	{
 		
@@ -241,49 +403,45 @@ int main()
 		glfwPollEvents();
 		glColor3f(0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		generateObject(x);
 		mnll* tptr = head;
 		
-		// h1.drawHouse();
-		//t1.drawTree();
+
 		while (tptr != NULL)
 		{
 			
-			//cout << "ok" << endl;
-			
-			//	cout << "ok" << endl;
+
 				if (tptr->isHouse)
 				{
-					
-					//house* h1 = tptr->houseNode;
-				//	cout << "house" << endl;
+
 					tptr->houseNode->drawHouse();
 					tptr->houseNode->posUpdate();
 				}
 				else {
 					
-					//tree* t1 = tptr->treeNode;
-				//	cout << "tree" << endl;
 					tptr->treeNode->drawTree();
-					 tptr->treeNode->posUpdate();
+					tptr->treeNode->posUpdate();
 				}
 			
 			tptr = tptr->next;
 			//cout << "ok" << endl;
+		
 		}
-		//Sleep(1);
+		
+		
+		m1.drawMan();
+		
 		glBegin(GL_LINES);
 		glLineWidth(2);
-		glVertex2f(0, 300);
-		glVertex2f(800, 300);
-		glVertex2f(0, 220);
-		glVertex2f(800, 220);
+		glVertex2f(0, 280);
+		glVertex2f(800, 280);
+		glVertex2f(0, 200);
+		glVertex2f(800, 200);
 
 		glEnd();
-		
+		m1.posUpdate();
 		glfwSwapBuffers(window);
-
-		x += 100;
+		
+		
 		if (head->isHouse)
 		{
 			if (head->houseNode->getFinX() < 0)
@@ -292,6 +450,7 @@ int main()
 				tptr = head;
 				head = head->next;
 				delete tptr;
+				generateObject(x);
 			}
 		}
 		else
@@ -302,11 +461,12 @@ int main()
 				tptr = head;
 				head = head->next;
 				delete tptr;
+				generateObject(x);
 			}
 
 		}
 		
-		
+		this_thread::sleep_for(chrono::milliseconds(41));
 	}
 	glfwTerminate();
 	return 0;
